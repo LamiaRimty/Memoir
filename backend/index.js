3c8e86e43a9adace959bbabd4c62a60f077962ab
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-12-24 21:42:22 +0100

    post a blog from backend

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,23 @@ app.get("/blogs",(req,res)=>{
     })
   })
 
+  app.post("/blogs",(req,res)=>{
+    const q= "INSERT INTO blogs (`image`, `title`, `time`, `qoute`, `desc`) VALUES (?)"
+    const values=[
+        "image from backend",
+        "title from backend",
+        "time from backend",
+        "qoute from backend",
+        "desc from backend",
+    ];
+
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Wow,Blog has been created!")
+      })
+})
+
+
 app.listen(8800,()=>{
     console.log("Connected to Memoir's backend!")
 })
